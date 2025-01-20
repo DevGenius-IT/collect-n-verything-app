c4732943e02d633427f2df206787ce7420441340
Kylian Bardini <kylianb1@icloud.com>
2025-01-20 10:34:50 +0100

    ➕🔧 chore/config: install tailwind, shadcn, i18n, lucide-icons modules and deps and configure app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+    'nuxt-lucide-icons',
+    '@nuxtjs/i18n'
+  ],
+  typescript: {
+    shim: true,
+  },
+  i18n: {
+    vueI18n: './i18n.config.ts',
+    locales: [
+      { code: 'fr', language: 'fr-FR' },
+      { code: 'en', language: 'en-US' },
+    ],
+    defaultLocale: 'fr',
+  },
+  app: {
+      pageTransition: { name: 'page', mode: 'out-in' },
+      layoutTransition: { name: 'layout', mode: 'out-in' },
+    },
+    css: ['~/assets/css/tailwind.css', '~/assets/css/fonts.css'],
+    experimental: {
+      viewTransition: true,
+    },
+  shadcn: {
+    /**
+      * Prefix for all the imported component
+      */
+    prefix: '',
+    /**
+      * Directory that the component lives in.
+      * @default "./components/ui"
+      */
+    componentDir: './components/ui'
+  }
 })
