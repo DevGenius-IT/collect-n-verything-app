fa4ef004574f468119e51de43c88e58f0f8d75eb
Kylian Bardini <kylianb1@icloud.com>
2025-02-17 09:20:16 +0100

    🚩💄 feat/style: update admin layout style

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -23,15 +23,7 @@
         <Sidebar />
         <Navbar />
         <div
-          :class="
-            cn(
-              'grid transition-all duration-300 ease-in-out flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8',
-              {
-                'sm:pl-64': !useDesign.state.toggleRetract,
-                'sm:pl-20': useDesign.state.toggleRetract,
-              },
-            )
-          "
+          class="grid transition-all duration-300 ease-in-out flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8"
         >
           <slot />
         </div>
