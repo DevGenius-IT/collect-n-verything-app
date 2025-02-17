f8574e58bc358aaa5754b57dcc2cbecb480976bc
Kylian Bardini <kylianb1@icloud.com>
2025-02-17 09:20:43 +0100

    🚩🛂 feat/perms: implement admin middleware

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="w-full flex justify-center">
-    <h1 class="text-2xl">Users</h1>
+    <h1 class="text-2xl">{{ t("users.title") }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-import { cn } from "~/lib/utils";
 const { t } = useI18n();
 
 definePageMeta({
   layout: "admin",
   title: "users.seo.title",
+  middleware: ["admin"],
 });
 
 const useDesign = designStore();
